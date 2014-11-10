5d4d78f4e3f27268678c4ead9ae2e11fd00d115d
Danny Ricciotti <dan.ricciotti@gmail.com>
2014-11-10 10:55:16 +0200

    more nonsense.

--- EvilBot.js
+++ EvilBot.js
@@ -1,6 +1,9 @@
 Twit = require('twit');
 fs = require('fs');
 _ = require('underscore');
+var RateLimiter = require('limiter').RateLimiter;
+var limiter = new RateLimiter(1, 780000);	// 780000 = 13 minutes
+
 
 function setupTwitter() {
 
@@ -51,7 +54,7 @@ function test(T) {
 	});
 }
 
-function retweet(T, params, callback) {
+function retweetBySearch(T, params, callback) {
 
   	T.get('search/tweets', params, function (err, reply) {
 		if(err) return callback(err);
@@ -122,16 +125,150 @@ function searchFollow(T, params, callback) {
 // }
 
 function favoriteTweet(T, tweet) {
+	
+	var username = getSafeUsername(tweet);
+	if ( username ) {
+		console.log('[FAV] '+tweet.id+'\t@'+username+' \t '+tweet.text);
+		T.post('favorites/create', { id: tweet.id_str }, function(error, response) {
+			if ( error ) {
+				console.log('[FAV] Error='+error);
+			} 
+		});
+	} else {
+		callback(new Error('[FAV] MyError=no username'));
+	}
+}
 
-	T.post('favorites/create', { id: tweet.id_str }, function(error, response) {
+function retweet(T, tweet, callback) {
 
-		if ( error ) {
-			console.log('ERROR: '+error);
-		} else {
-			console.log('favorited tweet: '+tweet.text);
+	var username = getSafeUsername(tweet);
+	if ( username ) {
+		console.log('[RT] '+tweet.id+'\t@'+username+' \t '+tweet.text);
+		T.post('statuses/retweet/:id', { id: tweet.id_str }, callback);
+	} else {
+		callback(new Error('[RT] MyError=no username'));
+	}
+}
+
+function followUser(T, tweet, callback) {
+	
+	var username = getSafeUsername(tweet);
+	if ( username ) {
+		console.log('[FOLLOW] @'+username+' \t Tweet=' +tweet.text); 		 		
+		T.post('friendships/create', { screen_name: username }, callback);
+	} else {
+		callback(new Error('[FOLLOW] MyError=no username'));
+	}
+}
+
+function logResponseIfError(error, reply) {
+	if ( error ) {
+		console.log('ERROR: ='+JSON.stringify(error));
+	}
+}
+
+function getSafeUsername(tweet) {
+	if ( tweet && tweet.user && tweet.user.screen_name ) {
+		return tweet.user.screen_name
+	}
+	return undefined;
+}
+
+function getCountHodors() {
+
+	var rand = _.random(1,32);
+	var f = Math.log(rand) / Math.LN2;
+	var g = 6 - Math.ceil(f);
+	// console.log('r='+rand+'  \t g='+g);
+	return g;
+}
+
+function buildHodor(times) {
+
+	var str = '';
+	for ( i = 0; i < times; i++ ) {
+		var rand = _.random(8);
+		str += 'Hodor';
+		if ( rand < 1 ) {
+			str += '?';
+		} else if ( rand < 2 ) {
+			str += '!';
+		} else if ( rand < 3 ) {
+			str += '!!';
+		} else if ( rand < 5 ) {
+			str += '.';
 		}
 
+		if ( i < times - 1 ) {
+			str += ' ';
+		}
+	}
+	return str;
+}
+
+function replyHodor(T, tweet, callback) {
+
+	var username = getSafeUsername(tweet);
+	if ( !username ) {
+		return callback(new Error('[REPLY] MyError=no username'));
+	}
+
+	var countHodors = getCountHodors();
+	var status = '@'+username+' '+buildHodor(countHodors);
+
+	var params = {
+		status: status,
+		in_reply_to_status_id : tweet.id
+	}
+
+	if ( ! params.in_reply_to_status_id ) {
+		console.log('[ERROR] Missing Reply_to '+JSON.stringify(tweet));
+	}
+
+	console.log('[HODOR] Replying to @'+username+' with: "'+status+'"');
+
+	T.post('statuses/update', params, function(error, reply) {
+		if ( error ) {
+			console.log('[HODOR] error='+error);
+		}
 	});
+}
+
+function respondToTweet(T, tweet) {
+
+	// console.log('');
+	// console.log('Saw tweet:'+JSON.stringify(tweet.text)+' user_id='+tweet.user.id_str);
+	var r = _.random(200);
+	var isGameInvite = (tweet.text.indexOf('evilapples.com/join') >= 0 );
+	var hashtagEvilApples = (tweet.text.indexOf('#evilapples') >= 0 );
+	var rt = tweet.retweeted_status;
+
+	// if ( isGameInvite ) {
+	// 	return favoriteTweet(T, tweet);
+	// }
+
+
+	if ( rt ) {
+		if (( rt.retweet_count > 4 && rt.retweet_count < 20 ) || ( rt.favorite_count > 4 && rt.favorite_count < 20)) {
+			console.log('Replying to a popular RT');
+			return replyHodor(T, tweet, logResponseIfError);
+		}
+	}
+
+
+	// if ( hashtagEvilApples ) {
+	// 	console.log('[HASHTAG] \t Tweet='+tweet.text);
+	// }
+
+	if ( r < 20 ) {
+		return retweet(T, rt, logResponseIfError); 
+	} else if ( r < 40 ) {
+		return favoriteTweet(T, tweet, logResponseIfError);
+	} else if ( r < 100 ) {
+		return followUser(T, tweet, logResponseIfError);
+	} else {
+		return replyHodor(T, tweet, logResponseIfError);
+	}
 }
 
 function EvilBot(configFile) {
@@ -140,14 +277,13 @@ function EvilBot(configFile) {
 	var config = setupConfig(configFile);
 	var twitter = setupTwitter();
 	var thisguy = {};
+	var tweetCount = 0;
+	var countPending = 0;
 
 	console.log('config='+JSON.stringify(config.keyword));
 
 	thisguy.twitter = twitter;
 	thisguy.test = function() { test(twitter) };
-	thisguy.retweet = function(params,callback) {
-		retweet(twitter, params, callback);
-	};
 	thisguy.searchFollow = function(params, callback) {
 		searchFollow(twitter, params, callback);
 	};
@@ -155,10 +291,21 @@ function EvilBot(configFile) {
 		console.log('starting stream ('+config.keyword+')...');
 		var stream = twitter.stream('statuses/filter', { track: config.keyword, lang: 'en', });
 		stream.on('tweet', function (tweet) {
-			// if ( tweet.media ) {
-  				console.log(JSON.stringify(tweet.text));
-  			// }
-  			// favoriteTweet(twitter, tweet);
+
+			if ( countPending >= 2 ) {
+				// console.log('['+limiter.getTokensRemaining()+'] Skipping tweet. Pending='+countPending);
+				return;
+			}
+			if ( tweet.user && tweet.user.id_str === config.user_id ) {
+				// console.log('Skipping you own tweet');
+				return;
+			}
+			var myTweetCount = ++tweetCount;
+			countPending++;
+			limiter.removeTokens(1, function(err, remainingRequests) {
+				countPending--;
+				respondToTweet(twitter, tweet);
+			});
 		});
 	};
 
--- index.js
+++ index.js
@@ -16,3 +16,31 @@ var b = require('./EvilBot.js')('./config.json');
 // });
 
 b.startStream();
+
+// for ( var i = 0 ; i < 32; i++ ) {
+// 	getCountHodors();
+// }
+
+// function buildHodor(times) {
+
+// 	var str = '';
+// 	for ( i = 0; i < times; i++ ) {
+// 		str += 'hodor';
+// 		if ( i < times - 1 ) {
+// 			str += ' ';
+// 		}
+// 	}
+// 	return str;
+// }
+
+
+// console.log(buildHodor(8)+'/');
+
+// function getCountHodors() {
+
+// 	var rand = _.random(1,32);
+// 	var f = Math.log(rand) / Math.LN2;
+// 	var g = 6 - Math.ceil(f);
+// 	console.log('r='+rand+'  \t g='+g);
+// 	return g;
+// }
